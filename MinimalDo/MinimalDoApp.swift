f6194b5d80d4497c54b3c00c7cc851b79381ab27
fto-k <fto@kovalee.app>
2024-08-06 17:39:33 +0200

    Initial Commit

---
+++ MinimalDo/MinimalDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalDoApp.swift
+//  MinimalDo
+//
+//  Created by Filippo Tosetto on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MinimalDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
