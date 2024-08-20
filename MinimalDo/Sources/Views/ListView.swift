1fadab61b730bf2e11615c67b5c4406b16bbbbce
Quy Duc <quyduc711@gmail.com>
2024-08-20 16:55:35 +0200

    add more details and button toggle to Item row

--- MinimalDo/Sources/Views/ListView.swift
+++ MinimalDo/Sources/Views/ListView.swift
@@ -1,18 +1,41 @@
 import SwiftUI
 
 struct ListView: View {
-    let todoList: TodoList
+    @State var todoList: TodoList
+
     var body: some View {
         List {
-            ForEach(todoList.todoItems) { todoItem in
+            ForEach($todoList.todoItems) { $todoItem in
                 HStack {
                     Image(systemName: todoItem.isDone
                           ? "largecircle.fill.circle"
                           : "circle")
-                      .imageScale(.large)
-                      .foregroundColor(.accentColor)
-                    Text(todoItem.title)
-                  }
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .onTapGesture {
+                        todoItem.isDone.toggle()
+                    }
+                    VStack(alignment: .leading) {
+                        Text(todoItem.title)
+                            .font(.headline)
+                        if let description = todoItem.description, !description.isEmpty {
+                            Text(description)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        if let dueDate = todoItem.dueDate {
+                            HStack {
+                                Image(systemName: "calendar")
+                                    .imageScale(.small)
+                                    .foregroundColor(.secondary)
+                                Text(dueDate, style: .date)
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            }
+                            .padding(.top, 2)
+                        }
+                    }
+                }
             }
         }
         .navigationTitle("\(todoList.name)")
